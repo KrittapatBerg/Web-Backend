a38f64d9d6b7ba4805f8fa8e100acfee58701b20
Gustav Berg <77542775+saldermorth@users.noreply.github.com>
2024-01-09 10:07:53 +0100

    make a petlist

--- AppGoodFriendsRazor/Pages/Friend/FriendInCountry.cshtml.cs
+++ AppGoodFriendsRazor/Pages/Friend/FriendInCountry.cshtml.cs
@@ -10,6 +10,7 @@ namespace AppGoodFriendsRazor.Pages.Friend
         readonly IFriendsService service;
 
         public List<FriendsByCountry> FriendsByCountries = new List<FriendsByCountry>();
+        public List<FriendPetByCity> FriendPetByCities = new List<FriendPetByCity>();
 
         //public List<IFriend> Friends { get; set; } = new List<IFriend>();
 
@@ -17,6 +18,7 @@ namespace AppGoodFriendsRazor.Pages.Friend
         {
             var info = await service.InfoAsync;
             var countryList = info.Friends;
+            var pet = info.Pets;
 
             var gangByCountry = countryList.GroupBy(x => x.Country);
             foreach (var gang in gangByCountry)
@@ -30,6 +32,29 @@ namespace AppGoodFriendsRazor.Pages.Friend
                 FriendsByCountries.Add(f);
             }
 
+            var petByCity = countryList.Join(
+                pet,
+                c => c.City,
+                p => p.City,
+                (c, p) => new
+                {
+                    City = c.City,
+                    NrFriends = c.NrFriends,
+                    NrPets = p.NrPets
+                });
+
+            var groupPetByCity = petByCity.GroupBy(x => x.City);
+            foreach (var group in groupPetByCity)
+            {
+                var petList = new FriendPetByCity();
+                petList.PetCities = group.Key;
+                petList.NrFriends = group.First().NrFriends;
+                petList.NrPets = group.Sum(x => x.NrPets);
+                petList.Pets = pet.Where(x => x.City == group.Key).ToList();
+
+                FriendPetByCities.Add(petList);
+            }
+
             return Page();
         }
 
@@ -39,6 +64,14 @@ namespace AppGoodFriendsRazor.Pages.Friend
             public int TotalFriends { get; set; }
             public int TotalCities { get; set; }
             public List<gstusrInfoFriendsDto> Cities { get; set; }
+        }
+
+        public class FriendPetByCity
+        {
+            public string PetCities { get; set; }
+            public int NrPets { get; set; }
+            public int NrFriends { get; set; }
+            public List<gstusrInfoPetsDto> Pets { get; set; }
         }
         #region Constructor 
         public FriendInCountryModel(IFriendsService service)
