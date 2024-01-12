bbede1bf808be3f0869dacf01f889f28f5d7f37b
Gustav Berg <77542775+saldermorth@users.noreply.github.com>
2024-01-12 14:42:07 +0100

    ListOfFriend page works, Have to work on NrPets and NrQuotes

---
+++ AppGoodFriendsRazor/Pages/EditDemo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppGoodFriendsRazor.Pages
+{
+    public class EditDemoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- AppGoodFriendsRazor/Pages/Friend/ListOfFriend.cshtml.cs
+++ AppGoodFriendsRazor/Pages/Friend/ListOfFriend.cshtml.cs
@@ -9,24 +9,86 @@ namespace AppGoodFriendsRazor.Pages.Friend
     public class ListOfFriendModel : PageModel
     {
         readonly IFriendsService service;
+        readonly ILogger<ListOfFriendModel> logger;
         loginUserSessionDto usr = null;
+        public List<IFriend> FriendsList { get; set; } = new List<IFriend>();
+        public List<IAddress> AddressList { get; set; } = new List<IAddress>();
+        IGrouping<IFriend, IAddress> Grouping { get; set; }
 
-
-        public List<IAddress> Addresses { get; set; }
-        public List<IFriend> FriendsList { get; set; }
-        public async Task<IActionResult> OnGetAsync()
+        //public List<csFriendListInCity> ListOfFriends { get; set; }
+        //public List<csFriendListInCity> ListNrQuotes { get; set; }
+        //public List<IAddress> AddressList { get; set; }
+        //public List<IPet> PetList { get; set; }
+        //public List<IQuote> QuoteList { get; set; }
+        //public List<csFriendListInCity> QuoteList { get; set; }
+        public class csFriendListInCity
         {
-            FriendsList = await service.ReadFriendsAsync(usr, true, false, "", 0, 100);
+            public string Cities { get; set; }
+            public int NrOfQuotes { get; set; }
+            public int NrOfPets { get; set; }
 
-            //var friendList = info.Friends.ToList();
+        }
+        //public string Country { get; set; }
+        //public string City { get; set; }
+
+        public async Task<IActionResult> OnGet() //string country, string city
+        {
+            //Country = country;
+            //City = city;
+
+            //var info = await service.InfoAsync;
+            //var friendInfo = info.Friends;
+            //var petInfo = info.Pets;
+
+            //var friendInfo = info.Friends.Where(x => x.City == city && x.City != null).ToList();
+            //var petInfo = info.Pets.Where(x => x.City == city && x.City != null).ToList();
+
+            var idList = Request.Query["idList"].ToString();
+
+            var friendsList = await service.ReadFriendsAsync(usr, true, false, "", 0, 100);
+            var inCity = friendsList.Where(x => x.Address != null).ToList();
+            var friendInCity = inCity.Where(x => x.Address.City == idList).ToList();
+            FriendsList = friendInCity;
+
+            //FriendsList = friendInfo;
+            //foreach (var item in friendInfo)
+            //{
+            //    var f = new csFriendListInCity();
+            //    f.Cities = item.
+            //    f.NrOfPets = f.NrOfPets;
+            //    f.NrOfQuotes = f.NrOfQuotes;
+
+            //    FriendsList.Add(f);
+            //}
             return Page();
+            //PetList = await service.ReadPetsAsync(null, true, false, "", 0, 100);
+
+
+
+
+            //var friendInfo = info.Friends
+            //                    .Where(x => x.Country == country && x.City == city
+            //                    && x.City != null).ToList();
+            ////.GroupBy(x => x.City != null)
+
+            //var petInfo = info.Pets
+            //                    .Where(x => x.Country == country && x.City != null)
+            //                    .GroupBy(x => x.City != null).ToList();
+
+
+            //AddressList = await service.ReadAddressesAsync(usr, true, false, "", 0, 100);
+            //PetList = await service.ReadPetsAsync(usr, true, false, country, 0, 100);
+            //QuoteList = await service.ReadQuotesAsync(usr, true, false, " ", 0, 100);
+            //petpet = PetList.Where()
         }
 
-
+        //TODO: include pets and quotes 
+        //TODO: filter list of friends in just one city 
         #region Inject service
-        public ListOfFriendModel(IFriendsService service)
+        public ListOfFriendModel(IFriendsService service, ILogger<ListOfFriendModel> logger)
         {
             this.service = service;
+            this.logger = logger;
         }
         #endregion
     }
