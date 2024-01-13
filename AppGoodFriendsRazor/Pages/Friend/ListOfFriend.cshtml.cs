48f80f3d21c624068cfc4a6800d45c2e12e97465
Gustav Berg <77542775+saldermorth@users.noreply.github.com>
2024-01-13 10:26:25 +0100

    List of Friend Done. Can wprk on pagination and search bar

--- AppGoodFriendsRazor/Pages/Friend/ListOfFriend.cshtml.cs
+++ AppGoodFriendsRazor/Pages/Friend/ListOfFriend.cshtml.cs
@@ -11,9 +11,9 @@ namespace AppGoodFriendsRazor.Pages.Friend
         readonly IFriendsService service;
         readonly ILogger<ListOfFriendModel> logger;
         loginUserSessionDto usr = null;
+
         public List<IFriend> FriendsList { get; set; } = new List<IFriend>();
         public List<IAddress> AddressList { get; set; } = new List<IAddress>();
-        IGrouping<IFriend, IAddress> Grouping { get; set; }
 
         //public List<csFriendListInCity> ListOfFriends { get; set; }
         //public List<csFriendListInCity> ListNrQuotes { get; set; }
@@ -21,13 +21,13 @@ namespace AppGoodFriendsRazor.Pages.Friend
         //public List<IPet> PetList { get; set; }
         //public List<IQuote> QuoteList { get; set; }
         //public List<csFriendListInCity> QuoteList { get; set; }
-        public class csFriendListInCity
-        {
-            public string Cities { get; set; }
-            public int NrOfQuotes { get; set; }
-            public int NrOfPets { get; set; }
+        //public class csFriendListInCity
+        //{
+        //    public string Cities { get; set; }
+        //    public int NrOfQuotes { get; set; }
+        //    public int NrOfPets { get; set; }
 
-        }
+        //}
         //public string Country { get; set; }
         //public string City { get; set; }
 
@@ -50,6 +50,7 @@ namespace AppGoodFriendsRazor.Pages.Friend
             var friendInCity = inCity.Where(x => x.Address.City == idList).ToList();
             FriendsList = friendInCity;
 
+            return Page();
             //FriendsList = friendInfo;
             //foreach (var item in friendInfo)
             //{
@@ -60,11 +61,8 @@ namespace AppGoodFriendsRazor.Pages.Friend
 
             //    FriendsList.Add(f);
             //}
-            return Page();
+
             //PetList = await service.ReadPetsAsync(null, true, false, "", 0, 100);
-
-
-
 
             //var friendInfo = info.Friends
             //                    .Where(x => x.Country == country && x.City == city
@@ -82,7 +80,7 @@ namespace AppGoodFriendsRazor.Pages.Friend
             //petpet = PetList.Where()
         }
 
-        //TODO: include pets and quotes 
+        //TODO: Work on search and Pagination if there's time.
         //Dont forget about the <a href> tag  
         #region Inject service
         public ListOfFriendModel(IFriendsService service, ILogger<ListOfFriendModel> logger)
